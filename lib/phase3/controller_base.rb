0b60aac3e3c20ae2bc19619db75232157bdc4e02
App Academy Student <student@appacademy.io>
2015-07-21 14:10:29 -0700

    finish phase III

--- lib/phase3/controller_base.rb
+++ lib/phase3/controller_base.rb
@@ -1,6 +1,7 @@
 require_relative '../phase2/controller_base'
 require 'active_support'
 require 'active_support/core_ext'
+require 'active_support/inflector'
 require 'erb'
 
 module Phase3
@@ -8,6 +9,9 @@ module Phase3
     # use ERB and binding to evaluate templates
     # pass the rendered html to render_content
     def render(template_name)
+      template_file = File.read("views/#{self.class.to_s.underscore}/#{template_name}.html.erb")
+      template = ERB.new(template_file).result(binding) # eval needed later on?
+      render_content(template, 'text/html')
     end
   end
 end
