77630d7b4d29b3661984a2a2816adc6c1c4085b2
apopma <adam.popma@gmail.com>
2015-07-22 15:06:36 -0700

    fix duplicate cookies being created with new paths

---
+++ lib/flash.rb
@@ -0,0 +1,30 @@
+class Flash
+  attr_reader :flash_now, :flash_later, :request_cookie
+
+  def initialize(req)
+    # pull out the proper cookie and store it, or a blank hash if no cookie
+    @request_cookie = req.cookies.find { |c| c.name == "_rails_lite_flash" }
+    @flash_now = @request_cookie ? JSON.parse(request_cookie.value) : {}
+    @flash_later = {}
+  end
+
+  def [](key)
+    # SETTING a flash can be either now or later,
+    # but viewing a flash in a view is always now, not later
+    flash_now[key]
+  end
+
+  def []=(key, val)
+    @flash_later[key] = val
+  end
+
+  def now
+    flash_now
+  end
+
+  def store(res)
+    new_cookie = WEBrick::Cookie.new("_rails_lite_flash", flash_later.to_json)
+    new_cookie.path = "/"
+    res.cookies << new_cookie
+  end
+end
