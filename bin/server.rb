066e86ea2623c17030cc69da4b4463014f52f282
App Academy Student <student@appacademy.io>
2015-07-22 12:03:52 -0700

    glob together server phases, add routes from phase 5

---
+++ bin/server.rb
@@ -0,0 +1,99 @@
+require 'active_support'
+require 'active_support/core_ext'
+require 'webrick'
+
+require_relative '../lib/controller_base'
+require_relative '../lib/router'
+
+# http://www.ruby-doc.org/stdlib-2.0/libdoc/webrick/rdoc/WEBrick.html
+# http://www.ruby-doc.org/stdlib-2.0/libdoc/webrick/rdoc/WEBrick/HTTPRequest.html
+# http://www.ruby-doc.org/stdlib-2.0/libdoc/webrick/rdoc/WEBrick/HTTPResponse.html
+# http://www.ruby-doc.org/stdlib-2.0/libdoc/webrick/rdoc/WEBrick/Cookie.html
+
+$cats = [
+  { id: 1, name: "Curie" },
+  { id: 2, name: "Markov" },
+  { id: 3, name: "Gizmo"}
+]
+
+$statuses = [
+  { id: 1, cat_id: 1, text: "Curie loves string!" },
+  { id: 2, cat_id: 2, text: "Markov is mighty!" },
+  { id: 3, cat_id: 1, text: "Curie is cool!" },
+  { id: 4, cat_id: 3, text: "Gizmo is secretly a cyborg!"}
+]
+
+class Cat
+  attr_reader :name, :owner
+
+  def self.all
+    @cat ||= []
+  end
+
+  def initialize(params = {})
+    params ||= {}
+    @name, @owner = params["name"], params["owner"]
+  end
+
+  def save
+    return false unless @name.present? && @owner.present?
+
+    Cat.all << self
+    true
+  end
+
+  def inspect
+    { name: name, owner: owner }.inspect
+  end
+end
+
+class StatusesController < ControllerBase
+  def index
+    statuses = $statuses.select do |s|
+      s[:cat_id] == Integer(params[:cat_id])
+    end
+
+    render_content(statuses.to_s, "text/text")
+  end
+end
+
+class CatsController < ControllerBase
+  # def index
+  #   render_content($cats.to_s, "text/text")
+  # end
+
+  def create
+    @cat = Cat.new(params["cat"])
+    if @cat.save
+      redirect_to("/cats")
+    else
+      render :new
+    end
+  end
+
+  def index
+    @cats = Cat.all
+    render :index
+  end
+
+  def new
+    @cat = Cat.new
+    render :new
+  end
+end
+
+router = Router.new
+router.draw do
+  get Regexp.new("^/cats$"), CatsController, :index
+  get Regexp.new("^/cats/new$"), CatsController, :new
+  post Regexp.new("^/cats$"), CatsController, :create
+  get Regexp.new("^/cats/(?<cat_id>\\d+)/statuses$"), StatusesController, :index
+end
+
+server = WEBrick::HTTPServer.new(Port: 3000)
+server.mount_proc('/') do |req, res|
+  route = router.run(req, res)
+end
+
+trap('INT') { server.shutdown }
+server.start
