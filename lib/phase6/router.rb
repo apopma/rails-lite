31c95f44568773b484b173960cfdf2f9e25da1ce
apopma <adam.popma@gmail.com>
2015-07-21 17:32:58 -0700

    pass phase VIa Route#matches? specs

--- lib/phase6/router.rb
+++ lib/phase6/router.rb
@@ -3,10 +3,16 @@ module Phase6
     attr_reader :pattern, :http_method, :controller_class, :action_name
 
     def initialize(pattern, http_method, controller_class, action_name)
+      @pattern = pattern
+      @http_method = http_method
+      @controller_class = controller_class
+      @action_name = action_name
     end
 
     # checks if pattern matches path and method matches request method
     def matches?(req)
+      req.request_method.downcase.to_sym == http_method &&
+      pattern =~ req.path
     end
 
     # use pattern to pull out route params (save for later?)
