650e7573d9038626e9d5026323ff7ad4c4f4c078
apopma <adam.popma@gmail.com>
2015-07-22 11:46:23 -0700

    combine ControllerBase phases

---
+++ lib/session.rb
@@ -0,0 +1,25 @@
+require 'json'
+require 'webrick'
+
+class Session
+  # find the cookie for this app
+  # deserialize the cookie into a hash
+  def initialize(req)
+    @request_cookie = req.cookies.find { |c| c.name == "_rails_lite_app" }
+    @cookie = @request_cookie ? JSON.parse(@request_cookie.value) : {}
+  end
+
+  def [](key)
+    @cookie[key]
+  end
+
+  def []=(key, val)
+    @cookie[key] = val
+  end
+
+  # serialize the hash into json and save in a cookie
+  # add to the responses cookies
+  def store_session(res)
+    res.cookies << WEBrick::Cookie.new("_rails_lite_app", @cookie.to_json)
+  end
+end
