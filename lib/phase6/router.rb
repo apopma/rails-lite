8e8b382a488e3354de0361bc177799156c5cb04b
App Academy Student <student@appacademy.io>
2015-07-21 17:42:01 -0700

    pass phase VIb specs

--- lib/phase6/router.rb
+++ lib/phase6/router.rb
@@ -25,10 +25,12 @@ module Phase6
     attr_reader :routes
 
     def initialize
+      @routes = []
     end
 
     # simply adds a new route to the list of routes
     def add_route(pattern, method, controller_class, action_name)
+      @routes << Route.new(pattern, method, controller_class, action_name)
     end
 
     # evaluate the proc in the context of the instance
@@ -39,10 +41,14 @@ module Phase6
     # make each of these methods that
     # when called add route
     [:get, :post, :put, :delete].each do |http_method|
+      define_method(http_method) do |pattern, controller_class, action_name|
+        add_route(pattern, http_method, controller_class, action_name)
+      end
     end
 
     # should return the route that matches this request
     def match(req)
+      @routes.find { |route| route.matches?(req) }
     end
 
     # either throw 404 or call run on a matched route
